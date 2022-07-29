69376ebea412d66fbefe7bd3c5e1b7b12390b249
Sindri Már Sigfússon <sindrisigfus@gmail.com>
2022-07-29 01:38:15 +0000

    text changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -672,7 +672,7 @@ const Home = (props: HomeProps) => {
                                 renderer={renderEndDateCounter}
                               />}
                             {wallet && isActive &&
-                              <h3>TOTAL MINTED : {itemsRedeemed} / {itemsAvailable}</h3>}
+                              <h3>TOTAL BOUGHT : {itemsRedeemed} / {itemsAvailable}</h3>}
                             {wallet && isActive && <BorderLinearProgress variant="determinate"
                                                                          value={100 - (itemsRemaining * 100 / itemsAvailable)}/>}
                             <br/>
